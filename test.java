99b7d2e7f05ffe9684d9e8441564ffa8751c00d7
federico.lopez <federico.lopez@estudiantes.utec.edu.uy>
2024-09-28 20:28:55 -0300

    Test 26

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 public class test {
     public static void main(String[] args) {
         // Imprime "Hola Mundo" en la consola
-        System.out.println("Hola Mundo,  ¿como estas?");
+        System.out.println("Hola Mundo,¿como estas?");
 
     }
 }
